e929a1dc0b9d30b41db6968fac3c3cbccd6b4fce
元嘉 <18897949405@163.com>
2017-03-20 17:14:35 +0800

    add background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-	styleUrls: ['./home.component.css','../../assets/headimg/css/demo.css','../../assets/headimg/css/style.css'],
+	styleUrls: ['./home.component.css'],
  })
 export class HomeComponent implements OnInit {
 
