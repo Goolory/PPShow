a5d041b3497919fd3e0e437c27b45eb0978486e6
元嘉 <18897949405@163.com>
2017-03-21 11:01:47 +0800

    add homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 declare var $: any;
+declare var WOW: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-	styleUrls: ['./home.component.css'],
+	styleUrls: ['./home.component.css','../../assets/homeimg/css/animate.css'],
  })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  		$(()=> {
+  	// 初始化加载的函数部分
+  	$(()=> {
 			$('#dg-container').gallery({
 				autoplay	:	true
 			});
 		});
+		// wow动态载入函数部分
+		var wow = new WOW({
+        animateClass: 'animated',
+        offset: 100,
+        callback: function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      });
+    wow.init();
+    
   }
 
 }
