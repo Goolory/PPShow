56f5e9ddae4862b33f029fda390246ec541d13f4
元嘉 <18897949405@163.com>
2017-03-22 17:43:17 +0800

    add album-add

--- src/app/album/album.module.ts
+++ src/app/album/album.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlbumComponent } from './album.component';
 import { RouterModule } from '@angular/router';
+import { AlbumAddComponent } from './album-add/album-add.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path:'',component:AlbumComponent}
+      { path:'',component:AlbumComponent},
+      { path:'add',component:AlbumAddComponent}
     ])
   ],
-  declarations: [AlbumComponent]
+  declarations: [AlbumComponent, AlbumAddComponent]
 })
 export class AlbumModule { }
