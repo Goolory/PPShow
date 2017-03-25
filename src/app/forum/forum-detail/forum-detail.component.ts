09493acdc14239b816c44cdb4a6aa08c6d5fe1b8
Yoon <15083500804@139.com>
2017-03-25 17:09:42 +0800

    fix forum-detail

--- src/app/forum/forum-detail/forum-detail.component.ts
+++ src/app/forum/forum-detail/forum-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-forum-detail',
   templateUrl: './forum-detail.component.html',
@@ -10,6 +10,19 @@ export class ForumDetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$(document).ready(function() {
+        $('#summernote').summernote({
+        	toolbar: [
+    //[groupname, [button list]]
+    ['图片',['picture']],
+    ['para', ['ul', 'ol', 'paragraph']],
+  ],
+  height: 300,                 // set editor height
+  minHeight: null,             // set minimum height of editor
+  maxHeight: null,             // set maximum height of editor
+  focus: false, 
+});
+    });
   }
 
 }
