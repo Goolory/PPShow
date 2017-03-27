c6a17c5b5dc5ca924c3fdb2ca9be54807b7c7671
元嘉 <18897949405@163.com>
2017-03-27 19:06:32 +0800

    add examdetail

---
+++ src/app/exam/exam-detail/exam-detail.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ExamDetailComponent } from './exam-detail.component';
+
+describe('ExamDetailComponent', () => {
+  let component: ExamDetailComponent;
+  let fixture: ComponentFixture<ExamDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExamDetailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExamDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/exam/exam.module.ts
+++ src/app/exam/exam.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExamComponent } from './exam.component';
-import { RouterModule} from '@angular/router'
+import { RouterModule} from '@angular/router';
+import { ExamDetailComponent } from './exam-detail/exam-detail.component'
  
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-    	{ path: '', component: ExamComponent}
+    	{ path: '', component: ExamComponent},
+    	{ path: 'detail', component: ExamDetailComponent}
     ])
   ],
-  declarations: [ExamComponent]
+  declarations: [ExamComponent, ExamDetailComponent]
 })
 export class ExamModule { }
