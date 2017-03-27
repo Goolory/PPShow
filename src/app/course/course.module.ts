f84f99e77a3487b08ed91117e026ad0fb1662f3c
元嘉 <18897949405@163.com>
2017-03-27 14:43:57 +0800

    add video-list

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CourseComponent } from './course.component';
-import { CourseVideoComponent } from './course-video/course-video.component'
+import { CourseVideoComponent } from './course-video/course-video.component';
+import { VideoListComponent } from './video-list/video-list.component'
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
     	{ path: '', component: CourseComponent},
+    	{ path: 'list', component: VideoListComponent},
     	{ path: 'video', component: CourseVideoComponent}
     ])
   ],
-  declarations: [CourseComponent, CourseVideoComponent]
+  declarations: [CourseComponent, CourseVideoComponent, VideoListComponent]
 })
 export class CourseModule { }
