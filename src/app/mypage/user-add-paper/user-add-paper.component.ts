471e6a5388d76632d8c54fb542a9884c87b061dd
元嘉 <18897949405@163.com>
2017-03-28 18:43:34 +0800

    add exam

--- src/app/mypage/user-add-paper/user-add-paper.component.ts
+++ src/app/mypage/user-add-paper/user-add-paper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-user-add-paper',
@@ -15,6 +16,32 @@ export class UserAddPaperComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+        $('#summernote').summernote({
+          toolbar: [
+          //[groupname, [button list]]
+          ['图片',['picture']],
+          ['para', ['ul', 'ol', 'paragraph']],
+        ],
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: false, 
+      });
+    });
+    var option_n = 2;
+    var options = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','H','I','J','K','L','M','N'];
+    $(".addOption").click(function(){
+      option_n++;
+      $("#option_answer").append('<label class="radio-inline"><input type="radio" name="inlineRadioOptions" value="option3"> '+options[option_n]+'</label>');
+    });
+    $(".resetOption").click(function(){
+      option_n = 2;
+      $("#option_answer").html("").append('<label class="radio-inline"><input type="radio" name="inlineRadioOptions" value="option3">A</label>'+
+        '<label class="radio-inline"><input type="radio" name="inlineRadioOptions" value="option3">B</label>'+
+        '<label class="radio-inline"><input type="radio" name="inlineRadioOptions" value="option3">C</label>');
+    });
+
   }
 
   public removePanel(){
